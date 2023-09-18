775e91a75b6cc404ac0dae6479f2a447b29bed92
Joaquin <138231434+joaquinrodriguezpou@users.noreply.github.com>
2023-09-18 12:40:05 -0300

    create a copy of calculation to operate

--- index.js
+++ index.js
@@ -3,15 +3,19 @@ let numberB = 0;
 let operator = 0;
 let calculation = [];
 let lastValue = calculation[calculation.length - 1];
-const screenResult = document.querySelector('.result');
+
 const numbers = Array.from(document.querySelectorAll('.num'));
 const operators = Array.from(document.querySelectorAll('.operator'));
 const clear = document.querySelector('.clear');
+const backSpace = document.querySelector('.backSpace');
 const equal = document.querySelector('.equal');
+const mainResult = document.querySelector('.main');
+const secondaryResult = document.querySelector('.secondary');
+mainResult.textContent = '0';
 
-const add = function(a, b) {    
+function add(a, b) {
     return a + b;
-};  
+}  
 
 const subtract = function(a, b) {
 	return a - b;;
@@ -19,7 +23,7 @@ const subtract = function(a, b) {
 
 const divide = function (a, b) {
     if (b === 0) {
-        return "Division by zero is not allowed.";
+        return "Division by zero? are you kidding?";
     }
     return a / b;
 };
@@ -48,15 +52,20 @@ const operate = function(numberA, operator, numberB) {
 }
 
 numbers.forEach(item => item.addEventListener('click', function(event){
+        let input = event.target.textContent;
+        if (input === '.') {
+            lastValue = Number(lastValue.toString() + input.toString());
+            calculation[calculation.length - 1] = lastValue;
+        }
         if(!isNaN(lastValue)){
-            lastValue = Number(lastValue.toString() + event.target.textContent.toString());
+            lastValue = Number(lastValue.toString() + input.toString());
             calculation[calculation.length - 1] = lastValue;
         }
         if(isNaN(lastValue)){
-            calculation.push(Number(event.target.textContent));
-            lastValue = (Number(event.target.textContent));
+            calculation.push(Number(input));
+            lastValue = (Number(input));
         };
-        screenResult.textContent = calculation.join(' ');
+        secondaryResult.textContent = calculation.join(' ');
 }))
 
 operators.forEach(operator => operator.addEventListener('click', function(event){
@@ -68,42 +77,59 @@ operators.forEach(operator => operator.addEventListener('click', function(event)
         lastValue = event.target.textContent;
         calculation[calculation.length - 1] = lastValue;
     };
-    screenResult.textContent = calculation.join(' ');
+    secondaryResult.textContent = calculation.join(' ');
 }))
 
 clear.addEventListener('click', function(){
     calculation = [];
     lastValue = calculation[calculation.length - 1];
-    screenResult.textContent = calculation;
+    secondaryResult.textContent = '';
+    mainResult.textContent = '0';
 })
+
+backSpace.addEventListener('click', function(){
+    calculation.pop();
+    if(calculation.length === 0){    
+        lastValue = '';
+        secondaryResult.textContent = '0';  
+    }
+    else{
+        lastValue = calculation[calculation.length - 1];
+        secondaryResult.textContent = calculation.join(' ');
+    }
+})
+
 const getResult = function() {
-    while (calculation.length >= 2) {
+    let calculationCopy = calculation.slice();
+    while (calculationCopy.length >= 2) {
         if (calculation.includes('x')) {
-            let indexX = calculation.indexOf('x');
-            let numberA = Number(calculation[indexX - 1]);
-            let operator = calculation[indexX];
-            let numberB = Number(calculation[indexX + 1]);
+            let indexX = calculationCopy.indexOf('x');
+            let numberA = Number(calculationCopy[indexX - 1]);
+            let operator = calculationCopy[indexX];
+            let numberB = Number(calculationCopy[indexX + 1]);
             let result = operate(numberA, operator, numberB);
-            calculation.splice(indexX - 1, 3, result); 
-        } else if (calculation.includes('/')) { 
-            let indexDivide = calculation.indexOf('/');
-            let numberA = Number(calculation[indexDivide - 1]);
-            let operator = calculation[indexDivide];
-            let numberB = Number(calculation[indexDivide + 1]);
+            calculationCopy.splice(indexX - 1, 3, result); 
+        } else if (calculationCopy.includes('/')) { 
+            let indexDivide = calculationCopy.indexOf('/');
+            let numberA = Number(calculationCopy[indexDivide - 1]);
+            let operator = calculationCopy[indexDivide];
+            let numberB = Number(calculationCopy[indexDivide + 1]);
             let result = operate(numberA, operator, numberB);
-            calculation.splice(indexDivide - 1, 3, result);
+            calculationCopy.splice(indexDivide - 1, 3, result);
         } else {
-            let numberA = Number(calculation[0]);
+            let numberA = Number(calculationCopy[0]);
             let operator = calculation[1];
-            let numberB = Number(calculation[2]);
+            let numberB = Number(calculationCopy[2]);
             let result = operate(numberA, operator, numberB);
-            calculation.splice(0, 3, result);
+            calculationCopy.splice(0, 3, result);
         }
     }
-    screenResult.textContent = calculation.join(' ');
+    mainResult.textContent = calculationCopy;
 }
 
 equal.addEventListener('click', function(){
+    if (!isNaN(lastValue)){
     getResult();
+    }
 })
 
